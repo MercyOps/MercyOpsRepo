dbf944761b5395b8031a47b48ea3ebee564024ee
MercyOps <32392103+MercyOps@users.noreply.github.com>
2017-09-29 09:57:07 -0400

    Add files via upload

---
+++ ModifyIO.ps1
@@ -0,0 +1,8 @@
+﻿Connect-VIServer -Server “vcentermgmt01” 
+$ESXiHosts = Get-Cluster “MEC01_UCS_QA/TEST” | Get-VMHost
+Write-Output $ESXiHosts
+ foreach ($ESXi in $ESXiHosts)
+ {
+ Get-VMhost $ESXi | Get-ScsiLun -LunType Disk | Where-Object {$_.CanonicalName -like 'naa.*' -and $_.MultipathPolicy -like ‘RoundRobin’} | Set-ScsiLun -CommandsToSwitchPath 1 | Out-File -Append "C:\ModifyIO-OUT.txt"
+ }
+ 
